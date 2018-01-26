a0bfb4390ad897b8fd3b286eea8882ca6687a1c5
Andy <andy9775@gmail.com>
2018-01-26 00:31:20 -0500

    change serve response from text to html
    Return HTML instead of plain text

--- src/zeit_crystal.cr
+++ src/zeit_crystal.cr
@@ -6,8 +6,20 @@ module ZeitCrystal
   def start
     port = (ENV["PORT"]? || "3000").to_i
     server = HTTP::Server.new("0.0.0.0", port) do |context|
-      context.response.content_type = "text/plain"
-      context.response.print "Hello world from Crystal Lang!"
+      context.response.content_type = "text/html"
+      context.response.print "
+        <!DOCTYPE html>
+          <html>
+          <head>
+            <meta charset=\"utf-8\">
+            <meta name=\"viewport\" content=\"width=device-width\">
+            <title>Crystal</title>
+          </head>
+          <body>
+            <div>Hello world from crystal lang and zeit!
+          </body>
+          </html>
+      "
     end
     puts "Starting server on port #{port}"
     server.listen
