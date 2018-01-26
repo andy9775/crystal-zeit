f7540194e6867c341d9d04dbb09b255fbf1e762c
Andy <andy9775@gmail.com>
2018-01-25 22:21:01 -0500

    setup basic web server

--- src/zeit_crystal.cr
+++ src/zeit_crystal.cr
@@ -1,6 +1,18 @@
 require "./zeit_crystal/*"
+require "http/server"
 
 # TODO: Write documentation for `ZeitCrystal`
 module ZeitCrystal
-  # TODO: Put your code here
+  def start
+    port = 3000
+    server = HTTP::Server.new(port) do |context|
+      context.response.content_type = "text/plain"
+      context.response.print "Hello world from Crystal Lang!"
+    end
+    puts "Starting server on port #{port}"
+    server.listen
+  end
 end
+
+include ZeitCrystal
+start
